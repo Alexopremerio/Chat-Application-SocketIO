6e33bb8cc90028fef83a7d0c3567de90621e5490
= <alec_1994@hotmail.com>
2018-01-26 19:55:16 +0100

    clients can send messages to each other (simple)

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,81 @@
-var ChatApp = (function () {
-    var socket;
+
+
+var ChatApp = (function(){
+
+    var _messageDOM = null;
+    var _sendDOM = null;
+    var _textDOM = null;
+    var _user = null;
+    const socket = io();
     function init(){
-        socket = io();
+        var params = [];
+       params = getParams(window.location.search.substr(1));
+       connectUser(params[0]);
+
+       _messageDOM = document.getElementById('messages');
+       _textDOM = document.getElementById('text');
+       _sendDOM = document.getElementById('send');
+       _sendDOM.addEventListener('click', function(){
+           console.log("CLICK");
+        sendMessage(_user,_textDOM.value);
+       })
+       
+    }
+
+    function connectUser(user){
+         _user = user;
+        socket.on('connect', function(){
+            
+            console.log('connected');
+            socket.emit('userJoin', _user);
+            socket.on('welcomeUser', function(msg){
+                console.log(msg);
+                printMessage(msg.from,msg.text);
+            });
         
-        socketIO();
+        });
+
+        socket.on('newMessage',function(msg){
+            printMessage(msg.from,msg.text);
+        });
+        console.log('ssdasds');
+          
+
+        socket.on('disconnect', function(){
+            console.log('you have been disconnected');
+        })
+
+    }
+
+    
+    function printMessage(from,text){
+        var li = document.createElement('li');
+        li.innerHTML = from + " : " + text;
+        console.log(li);
+        _messageDOM.appendChild(li);
         
     }
 
-    function socketIO(){
-        console.log(socket);
-        console.log('this: ' +this);
-        socket.emit('helllo', 'heelll');
-
-
+    function sendMessage(_from,_text){
+        var msg = {
+            from: _from,
+            text: _text
+        }
+        socket.emit('sendMessage', msg);
     }
+
+    function newMessage(msg){
+        console.log(msg);
+    }
+
+
+    function getParams(uri){
+        var temp = [];
+        uri.split("&").forEach(function(item){temp.push(item.split("=")[1]);}); 
+        return temp;
+    }
+
+
     return {
         init: init
     };
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const socketIO = require('socket.io');
 
 
 
-// process.env.PORT för Heroku 
+
 const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
@@ -19,11 +19,26 @@ var server = http.createServer(app);
 
 var io = socketIO(server);
 io.on('connection', (socket) => {
+    var username;
     console.log(' Connected');
-    socket.on('helllo', (data) => {
-        console.log(data);
-    })
+    socket.on('userJoin', (data) => {
+        
+        username = data;
+        console.log(`user ${username} connected`);
+        socket.emit('welcomeUser', ({
+            from: 'Admin',
+            text: `Welcome ${username}.`
+        }));
+        socket.broadcast.emit('welcomeUser', { from: 'Admin', text: `${username} joined the room`})
+    });
 
+    socket.on('sendMessage', (msg) => {
+        socket.emit('newMessage', msg);
+        socket.broadcast.emit('newMessage', msg);
+    })
+    
+    
+     
     socket.on('disconnect', () => {
 
     })
